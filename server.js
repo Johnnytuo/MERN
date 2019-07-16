ee4eb3449f0f7d9592c431f900f4c53bdde9c74f
tuo <tuos@andrew.cmu.edu>
2019-07-16 00:19:15 -0700

    added models

--- server.js
+++ server.js
@@ -8,23 +8,23 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
